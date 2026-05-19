19eea52b978591b82f1f377b246df8847277e23e
Gevans4352 <emogoghena@gmail.com>
2026-05-19 16:53:00 +0100

    fixing again

--- Backend/database.js
+++ Backend/database.js
@@ -1,9 +1,6 @@
 const { Sequelize } = require("sequelize");
-const Database = require("better-sqlite3");
-
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  dialectModule: Database,
   storage: "/tmp/urls.db"
 });
 
