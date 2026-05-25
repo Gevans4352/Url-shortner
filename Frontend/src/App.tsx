872fe4fba5ff4fe8895396e09c5be90126793469
Gevans4352 <emogoghena@gmail.com>
2026-05-25 23:05:56 +0100

    Update app config, styles and frontend dependencies

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,15 +4,43 @@ import NotFound from "./Pages/NotFound";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import ForgotPassword from "./Pages/ForgotPassword";
+import { useEffect, useState } from "react";
+import axios from "axios"
+import ResetPassword from "./Pages/ResetPassword";
 
 const App = () => {
+  const [user, setUser] = useState(null)
+  const [error, setError] = useState("")
+
+  useEffect(()=>{
+    const fetchUser = async ()=>{
+      const token = localStorage.getItem("token");
+      if(token){
+        try{
+          const res = await axios.get("api/users/me", {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          })
+          setUser(res.data)
+        
+        } catch(error){
+          setError("Failed to fetch user data")
+          localStorage.removeItem("token")
+        }
+      }
+    }
+    fetchUser()
+  }, [])
   return (
+
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/reset" element={<ForgotPassword/>} />
+        <Route path="/restart" element={<ResetPassword/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      "/api": "http://localhost:5000",
       '/shortUrls': 'http://localhost:5000'
     }
   }
