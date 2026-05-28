67afce9b0ab4dd1d71eeba777fe9f4fa01a7a07f
Gevans4352 <emogoghena@gmail.com>
2026-05-28 14:48:42 +0100

    feat: add protected route and forgot/reset password routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,42 +5,41 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import ForgotPassword from "./Pages/ForgotPassword";
 import { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 import ResetPassword from "./Pages/ResetPassword";
 
 const App = () => {
-  const [user, setUser] = useState(null)
-  const [error, setError] = useState("")
+  const [user, setUser] = useState(null);
 
-  useEffect(()=>{
-    const fetchUser = async ()=>{
+  useEffect(() => {
+    const fetchUser = async () => {
       const token = localStorage.getItem("token");
-      if(token){
-        try{
-          const res = await axios.get("api/users/me", {
+      if (token) {
+        try {
+          const res = await axios.get("https://url-shortner-c1kw.onrender.com/api/users/me", {
             headers: {
-              Authorization: `Bearer ${token}`
-            }
-          })
-          setUser(res.data)
-        
-        } catch(error){
-          setError("Failed to fetch user data")
-          localStorage.removeItem("token")
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setUser(res.data);
+        } catch (error) {
+          localStorage.removeItem("token");
         }
       }
-    }
-    fetchUser()
-  }, [])
+    };
+    fetchUser();
+  }, []);
   return (
-
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={user ? <Home /> : <Login setUser={setUser} />}
+        />
         <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/reset" element={<ForgotPassword/>} />
-        <Route path="/restart" element={<ResetPassword/>} />
+        <Route path="/reset" element={<ForgotPassword />} />
+        <Route path="/restart/:token" element={<ResetPassword />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
