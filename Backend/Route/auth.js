443fff608a4e410b79ff7264f54aa5462cac60d1
Gevans4352 <emogoghena@gmail.com>
2026-06-04 21:57:50 +0100

    removed redundant code

--- Backend/Route/auth.js
+++ Backend/Route/auth.js
@@ -3,15 +3,7 @@ const User = require("../Models/User");
 const protect = require("../Middleware/auth");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const nodemailer = require("nodemailer");
-const crypto = require("node:crypto");
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+
 
 const router = express.Router();
 //register
